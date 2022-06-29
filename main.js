a40c1e23c8f4d03ddc3c46ee887960b3d3743a77
rushii1192 <hrushikeshborakhede1089@gmail.com>
2022-06-29 12:26:49 +0530

    final completion of project

--- main.js
+++ main.js
@@ -2,12 +2,9 @@
 async function appendAddressData() {
     const response = await fetch('http://127.0.0.1:5500/data/city-data-min.json');
     const data = await response.json();
-
+    
     var selected_country = $('#country').find(":selected").text();
     var selected_state = $('#state').find(":selected").text();
-
-    $("#state").empty();
-    $("#city").empty();
     
     for(var country_counter=0;country_counter<data.length;country_counter++){
         $("#country").append('<option value='+data[country_counter].id+'>'+data[country_counter].name+'</option>');
@@ -30,3 +27,17 @@ $( document ).ready(function() {
     $("#state").change(appendAddressData);
     appendAddressData();
 });
+
+function showDetails(){
+    var country = $('#country').find(":selected").text();
+    var state = $('#state').find(":selected").text();
+    var city = $('#city').find(":selected").text();
+
+    alert(`
+    Address Line 1:- ${$("#addressLine1").val()}\n
+    Address Line 2:- ${$("#addressLine2").val()}\n
+    Country:- ${country}\n
+    State:- ${state} \n
+    City:- ${city}
+    `);
+}
